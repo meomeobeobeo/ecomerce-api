1a926ae7db6bf38ba8cc940015f53db611ee7388
ducnm <meotrangbeonknd@gmail.com>
2023-12-23 08:34:12 +0700

    add config crypography

--- src/helper/helper.service.ts
+++ src/helper/helper.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@nestjs/common'
 import * as moment from 'moment'
 import * as nodemailer from 'nodemailer'
+import * as CryptoJS from 'crypto-js'
+
 @Injectable()
 export class HelperService {
     //dateFactor đại diện cho một giá trị dựa trên thời gian, có thể mang tính duy nhất.
@@ -35,6 +37,35 @@ export class HelperService {
         let info = await transporter.sendMail(mailOptions)
 
         console.log('Email sent: ' + info.response)
+    }
+    encryptAes = (raw: string, key: string): string => {
+        try {
+            let encryptKey = CryptoJS.enc.Utf8.parse(key)
+
+            var data = CryptoJS.AES.encrypt(raw, encryptKey, {
+                mode: CryptoJS.mode.ECB,
+                padding: CryptoJS.pad.Pkcs7,
+            }).toString()
+            // rawData = JSON.parse(rawData);
+            return data
+        } catch (e) {
+            console.log(e)
+        }
+    }
+
+    decryptAes = (data: string, key: string): string => {
+        try {
+            let decryptKey = CryptoJS.enc.Utf8.parse(key)
+
+            var rawData = CryptoJS.AES.decrypt(data, decryptKey, {
+                mode: CryptoJS.mode.ECB,
+                padding: CryptoJS.pad.Pkcs7,
+            }).toString(CryptoJS.enc.Utf8)
+            // rawData = JSON.parse(rawData);
+            return rawData
+        } catch (e) {
+            console.log(e)
+        }
     }
 
     isEmail = (email: string): boolean => {
